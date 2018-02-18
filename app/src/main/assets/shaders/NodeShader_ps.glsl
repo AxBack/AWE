e7c8f2a43d73a77b561712bb0b7f86d6c0f4e3a7
Alexander Backenhof <alexander.backenhof@gmail.com>
2018-02-18 20:08:23 +0100

    tried to make nodes better

--- app/src/main/assets/shaders/NodeShader_ps.glsl
+++ app/src/main/assets/shaders/NodeShader_ps.glsl
@@ -11,5 +11,7 @@ layout(location = 0) out vec4 color1;
 void main() {
     float l =  length(uv) / max;
     color1 = color * (l > 0.5 ? 0.0f : 1.0f);
+    //color1 = color * (l > 0.5 ? 0.0f : smoothstep(-max*0.5, max, l));
+    //color1 = color * (1.0 - smoothstep(0.0, max, l));
     gl_FragDepth = color1.a > 0.0 ? gl_FragCoord.z : 1.0;
 }
